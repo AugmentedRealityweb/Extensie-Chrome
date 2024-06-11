7d8b7487784872117348fb1e626c6f641a7110ec
AugmentedRealityweb <160763410+AugmentedRealityweb@users.noreply.github.com>
2024-06-11 13:51:38 +0300

    Update background.js

--- background.js
+++ background.js
@@ -2,24 +2,7 @@ chrome.action.onClicked.addListener((tab) => {
   if (!tab.url.startsWith("chrome://")) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: injectCanvas
+      files: ['injectCanvas.js']
     });
   }
 });
-
-function injectCanvas() {
-  const iframe = document.createElement('iframe');
-  iframe.style.position = 'fixed';
-  iframe.style.top = 0;
-  iframe.style.left = 0;
-  iframe.style.width = '100vw';
-  iframe.style.height = '100vh';
-  iframe.style.zIndex = 9999;
-  iframe.style.border = 'none';
-  iframe.src = chrome.runtime.getURL('canvas.html');
-  document.body.appendChild(iframe);
-
-  setTimeout(() => {
-    document.body.removeChild(iframe);
-  }, 3000);
-}
