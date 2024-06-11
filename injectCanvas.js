30d133ee53d30e6ed6648905159971d4b0178b57
AugmentedRealityweb <160763410+AugmentedRealityweb@users.noreply.github.com>
2024-06-11 15:23:44 +0300

    Update injectCanvas.js

--- injectCanvas.js
+++ injectCanvas.js
@@ -17,7 +17,7 @@ function createEggGame() {
 
   let score = localStorage.getItem('eggScore') ? parseInt(localStorage.getItem('eggScore')) : 9999999;
   const egg = new Image();
-  egg.src = 'ou.png'; // Replace with the path to your egg image
+  egg.src = 'images/ou.png'; // Replace with the path to your egg image
 
   const eggWidth = 100;
   const eggHeight = 150;
