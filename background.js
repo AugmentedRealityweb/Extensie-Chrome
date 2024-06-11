835d826db1d504f9ad578e522a0f1709d60d017c
AugmentedRealityweb <160763410+AugmentedRealityweb@users.noreply.github.com>
2024-06-11 18:01:05 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
 chrome.action.onClicked.addListener((tab) => {
-  if (!tab.url.startsWith("chrome://")) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ['injectCanvas.js']
-    });
-  }
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['injectCanvas.js']
+  });
 });
